1acaf020584636b1343a50e57bb8bee0f680f8f4
inzaky <inzaky2000@yahoo.com>
2023-04-14 21:55:31 +0700

    compositionAPI

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
+//การเขียนแบบใหม่
 <template>
-  <h1>{{ title }}</h1>
+  <!-- <h1>{{ title }}</h1>
   <h2>{{ description }}</h2>
-  <h3>{{ year }}</h3>
+  <h3>{{ year }}</h3> -->
+  <HomeView />
+
+</template>
+<script setup lang="ts">
+    // import { ref } from 'vue';
+    import HomeView from './components/HomeView.vue';
+
+    // const title = ref("Hello World")
+    // const description = ref("How are you")
+    // const year = ref(2023)
+</script>
+
+<style scoped>
+
+</style>
+
+//ข้างล่างเป็นการเขียนแบบเดิม
+<!-- <template>
+  <h1>{{ data.title }}</h1>
+  <h2>{{ data.description }}</h2>
+  <h3>{{ data.year }}</h3>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref,reactive } from 'vue'
 
 export default defineComponent({
   // แบบใหม่ เป็น Composition api
   setup () {
-    const title = ref("Hello World")
-    const description = ref("How are you")
-    const year = ref(2023)
+    // const title = ref("Hello World")
+    // const description = ref("How are you")
+    // const year = ref(2023)
+    // console.log(title)
 
-    console.log(title)
+    // Reactive จะรองรับฉบับ objects เท่านั้น
+    const data = reactive ({
+      title: "Hello World!",
+      description: "welcome evevrybody",
+      year: 2022
+    })
+
+    console.log(data)
 
     setTimeout(() => {
-      title.value = "Hello Inzaky"
+      // title.value = "Hello Inzaky"
+      data.title = "Hello Peter"
     }, 2000)
 
-    return {title, description, year}
+    // return {title, description, year}
+    return { data }
   },
+
 
   // แบบเดิม เป็น Option api
   // data(){
@@ -40,4 +73,4 @@ export default defineComponent({
 
 <style scoped>
 
-</style>
+</style> -->
---
+++ src/components/HomeView.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+    <main>
+        <button @click = "counter++">up</button>
+        <h3>{{ counter }}</h3>
+        <button @click = "counter--">down</button>
+        <h3>{{ person.name }} {{ person.age }}</h3>
+        <button @click="updatePerson">update person</button>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+    import { reactive, ref } from "vue";
+    const counter = ref(0)
+
+    const person = reactive({name:"inzaky",age:47})
+
+    const updatePerson = () => {
+        person.name = "Ammar",
+        person.age = 45
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
